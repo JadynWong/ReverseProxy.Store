b91c6ca0c8eccc4123b10d897df44e940973e8e5
Jadyn <jadyn.wong@live.com>
2022-12-13 21:17:41 +0800

    Improve webapi DI

--- ReverseProxy.WebApi/Program.cs
+++ ReverseProxy.WebApi/Program.cs
@@ -1,14 +1,14 @@
-using ReverseProxy.WebApi.Validator;
 using FluentValidation;
 using FluentValidation.AspNetCore;
-using ReverseProxy.Store.Entity;
-using ReverseProxy.Store.EFCore;
 using Microsoft.EntityFrameworkCore;
-using ReverseProxy.Store.EFCore.Management;
-using Newtonsoft.Json;
 using Microsoft.OpenApi.Models;
-using Yarp.ReverseProxy.Model;
+using Newtonsoft.Json;
 using ReverseProxy.Store.Distributed.Redis;
+using ReverseProxy.Store.EFCore;
+using ReverseProxy.Store.EFCore.Management;
+using ReverseProxy.Store.Entity;
+using ReverseProxy.WebApi.Validator;
+using Yarp.ReverseProxy.Model;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,7 +20,6 @@ builder.Services.AddMemoryCache();
 builder.Services.AddSingleton<IValidator<Cluster>, ClusterValidator>();
 builder.Services.AddSingleton<IValidator<ProxyRoute>, ProxyRouteValidator>();
 
-
 //mysql
 //builder.Services.AddDbContext<EFCoreDbContext>(options =>
 //        options.UseMySql(
@@ -31,26 +30,42 @@ builder.Services.AddSingleton<IValidator<ProxyRoute>, ProxyRouteValidator>();
 builder.Services.AddDbContext<EFCoreDbContext>(options =>
         options.UseSqlServer(
             builder.Configuration.GetConnectionString("Default"),
-            b => b.MigrationsAssembly("ReverseProxy.WebApi")));
+            b =>
+            {
+                b.MigrationsAssembly("ReverseProxy.WebApi")
+                 .UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery);
+            }));
 
 builder.Services.AddTransient<IClusterManagement, ClusterManagement>();
 builder.Services.AddTransient<IProxyRouteManagement, ProxyRouteManagement>();
+builder.Services.AddFluentValidationAutoValidation().AddFluentValidationClientsideAdapters();
 builder.Services.AddControllers()
-    .AddFluentValidation()
-    .AddNewtonsoftJson(options => {
-                     options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
-                     options.SerializerSettings.DateFormatString = "yyyy-MM-dd HH:mm:ss";
-                 }); ;
+    .AddNewtonsoftJson(options =>
+    {
+        options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+        options.SerializerSettings.DateFormatString = "yyyy-MM-dd HH:mm:ss";
+    });
 
 builder.Services.AddReverseProxy()
     .LoadFromEFCore()
-    .AddRedis("127.0.0.1:6379");
-    ;
+    .AddRedis(builder.Configuration.GetConnectionString("Redis"));
+;
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "ReverseProxy.WebApi", Version = "v1" });
+});
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials()
+            .SetIsOriginAllowed(_ => true);
+    });
 });
 
 var app = builder.Build();
@@ -61,14 +76,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ReverseProxy.WebApi v1"));
 }
-app.UseCors(builder => {
-    builder
-        .AllowAnyOrigin()
-        .AllowAnyMethod()
-        .AllowAnyHeader()
-        //.AllowCredentials()
-        ;
-});
+app.UseCors();
 app.UseRouting();
 app.UseAuthorization();
 app.UseEndpoints(endpoints =>
